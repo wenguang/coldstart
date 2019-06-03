f6c0929455a553ce2f32341c08b7e2b147e7a616
wenguangpan <ivorygx@126.com>
2019-06-03 21:31:35 +0800

    mod

---
+++ coldstart-java-sso/src/main/java/com/zheng/upms/client/shiro/filter/RestAuthenticationFilter.java
@@ -0,0 +1,18 @@
+package com.zheng.upms.client.shiro.filter;
+
+import org.apache.shiro.web.filter.authc.AuthenticationFilter;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class RestAuthenticationFilter extends AuthenticationFilter {
+    @Override
+    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) {
+        return true;
+    }
+
+    @Override
+    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) {
+        return false;
+    }
+}
